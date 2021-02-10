a5799a1c305afdfce97c9f460e6d573652f65c7f
RoyJax579 <onslaught223@gmail.com>
2021-02-10 08:42:45 -0500

    if statements n for loop

--- test.py
+++ test.py
@@ -1,12 +1,4 @@
-num1 = 1.234
-num2 = 4.32
+people = [2, 3, 5, 12, 43, 54, 331, 1222]
 
-#regular formatting
-print("Num 1 is", num1, "and num 2 is", num2)
-
-#using the format method
-print("Num 1 is {1:.2f} and num 2 is {0:.2f}".format(num1, num2))
-
-#using f-string
-print(f"Num 1 is {num1:.4f} and num 2 is {num2}")
-
+for x in people:
+    print(x)
