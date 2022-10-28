d5f2508652f866769d3463290bfe9cf8f48d18f6
Roynickson Jacques <onslaught223@gmail.com>
2022-10-27 20:02:25 -0400

    Saw different methods that can be used with string

---
+++ String2.py
@@ -0,0 +1,21 @@
+msg = "Roynickson Jacques"
+
+#to get string length
+print(len(msg))
+
+#to get the characters of a string
+print(msg[0])
+
+#to get a range of characters in a string
+print(msg[0:11])
+
+#to get string to all lower case characters
+#It is the upper fot all upper case letters
+print(msg.lower())
+print(msg.upper())
+
+#to find number of characters in a string
+print(msg.count(""))
+
+#to find number of a specific character
+print(msg.count("a"))
