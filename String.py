5c4194cf0402a16c2d5880cfdea4f4d627f73647
Roynickson Jacques <onslaught223@gmail.com>
2022-10-29 19:26:30 -0400

    Changing file name from Math branch

--- String.py
+++
@@ -1,9 +0,0 @@
-# Working with textual data
-
-#3 double quotes is used to add text with multiple lines.
-msg = """Hello, 
-World!"""
-
-#Use plus to add text with text like Javascripts.
-print(msg+" of Warcraft III")
-
