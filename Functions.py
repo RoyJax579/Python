5b77a94fd0f40bb361b10c629077a4ca68ccacea
Roynickson Jacques <onslaught223@gmail.com>
2022-10-30 15:24:31 -0400

    Function and methods

---
+++ Functions.py
@@ -0,0 +1,17 @@
+#function
+x = max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+
+print(x)
+
+name = "sonny"
+#method
+print(name.upper())
+
+#methods are functions that are used with an object while functions are not.
+
+full_name = "ADams SandlERXXX"
+
+#title() capitalizes the first letter after each space
+
+# strip() removes any letter passed in it
+print(full_name.title().strip("x"))
--- Math_Exercise.py
+++
@@ -1,3 +0,0 @@
-num_one = 10
-num_one += 20
-print(num_one)
